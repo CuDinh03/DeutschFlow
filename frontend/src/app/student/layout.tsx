178e2f5b0c4a33cb65ef77b0b3507f135c5ec7f6
Đinh Cự <dinhhuycu0305@gmail.com>
2026-05-11 20:32:29 +0700

    fix(ui): move LanguageSwitcher to bottom-right - no longer overlaps header notifications

--- frontend/src/app/student/layout.tsx
+++ frontend/src/app/student/layout.tsx
@@ -5,7 +5,7 @@ import LanguageSwitcher from '@/components/ui/LanguageSwitcher'
 export default function StudentLayout({ children }: { children: React.ReactNode }) {
   return (
     <div className="relative min-h-screen">
-      <div className="fixed top-3 right-[80px] z-[60]">
+      <div className="fixed bottom-20 right-4 z-[60] lg:bottom-6 lg:right-6">
         <LanguageSwitcher />
       </div>
       {children}
